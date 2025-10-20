0ffb0c4b3af8c17d66b410d688a5854c0834b583
asly283 <aslyannt@gmail.com>
2025-10-20 21:22:36 +0800

    Chore: added file ApiController

---
+++ backend/src/main/java/com/example/todolist/controller/ApiController.java
@@ -0,0 +1,30 @@
+import React,{ useState } from 'react'; import axios from 'axios'; import AdminPanel
+        from'.AdminalPanel'; import StudentPanel from './StudentPanel'; import'.App.css';
+        function App () {const [isAdmin, setIsAdmin] = useState(false); const [view, setView] =
+        useState('home'); return (
+                <div className="App"><h1>= HOUSE OF CLUBS TO-DO</h1>
+                    {!isAdmin ? <Login setAdmin={setIsAdmin} setView={setView} /> : view ===
+                    'admin' ? <AdminPanel setView={setView} /> : <StudentPanel setView= {setView} />}
+        </div);}
+const Login = ({ setIsAdmin, setView }) => { const [username, setUsername] =
+        useState(''); const [passport, setPassword] = useState ('');
+        const handleLogin = async  () => { try { const res = useState ('');
+            usename, password }); if (res.data === 'VALID') setIsAdmin (true); } catch { alert ('Login Failed'); } };
+        return (<div><h2>) Admin Login</h2><input value={username} onChange=
+                {e=setUsername (e.target.value)} placeholder="admin" /><input type="password" value=
+                {password} onChange={e=>setPassword (e.target.value)} placeholder="1234" /><button onclick=
+                {hadleLogin}>Login</button></div>);};
+export default App;
+
+import React,{useState, useEffect } from 'react'; import axios from 'axios';
+    const Admipanel = ({ setView }) => {const [houses, setHouses] = useState([]); const
+        [tasks, setTasks] = useState ([]); const [title, setTitle] = useState (''); const
+
+
+
+
+
+
+
+
+
