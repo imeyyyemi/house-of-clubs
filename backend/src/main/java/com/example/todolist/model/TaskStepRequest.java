bdcea4af84a11fa3089c7ef5c1e08da9ab1e379c
romena <romena.laurente.jfmsntsg@gmail.com>
2025-10-20 21:42:51 +0800

    Chore: add file

--- backend/src/main/java/com/example/todolist/model/TaskStepRequest.java
+++ backend/src/main/java/com/example/todolist/model/TaskStepRequest.java
@@ -4,8 +4,10 @@ public class TaskStepRequest {
     public Long stepId;
     public boolen completed;
 
-    public TaskStepRequest() {}
-    public TaskStepRequest(Long stepId, boolen completed){
+    public TaskStepRequest() {
+    }
+
+    public TaskStepRequest(Long stepId, boolen completed) {
         this.stepId = stepId;
         this.completed = completed;
 
